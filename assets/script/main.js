c8135943b2342b364bbc59d62240c360f223bf9e
Hasan Atki <djamblers@gmail.com>
2022-01-23 02:44:12 +0000

    add all the wrong stuff to main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,31 @@
-console.log('script is loaded')
+document.getElementById('year').innerText.replace("0000", getFullYear());
+
+var menu = document.getElementsByClassName('menu');
+var menuItem = document.getElementsByClassName('menu-item-name');
+var menuItemPrice = document.getElementsByClassName('menu-item-price');
+var submitFamMeal = document.getElementsByClassName('submit-family-meal');
+var cartItem = document.getElementsByClassName('cart-item');
+var clearFromCartBtn = document.getElementsByClassName('clear-btn');
+var cartRow = document.getElementsByTagName('tr');
+
+for (var i = 0; i < menuItem.length || i < cartItem.length; i++) {
+    var button = menuItem[i];
+    button.addEventListener('click', () => {
+        cartRow[0].innerText = `
+            <tr>
+            <td class="item-name cart-item">${}</td>
+            <td><input class="quantity-value" type="number" min="1"></td>
+            <td class="price-value">${}</td>
+            <td><input class="clear-btn" type="button" value="&times;"></td>
+        </tr>`
+        });
+    
+    
+}
+
+function removeItemFromCart() {}
+function showDropDown() {}
+function hideDropDown() {}
+function calculateSubtotal() {}
+function calculateVat() {}
+function calculateTotalPrice() {}
