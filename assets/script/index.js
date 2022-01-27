9c589884e5cb6be7c59654e968a160e86a5765d6
Hasan Atki <djamblers@gmail.com>
2022-01-27 01:48:00 +0000

    split javascript into two separate files: homepage and shop page

---
+++ assets/script/index.js
@@ -0,0 +1,28 @@
+// Showing / Hiding elements
+
+const cta = document.getElementsByClassName('cta');
+const heroSection = document.getElementsByClassName('hero-container');
+const menu = document.getElementsByClassName('menu');
+
+cta[0].addEventListener('click', _ => {
+    menu[0].classList.toggle('hide');
+    heroSection[0].classList.toggle('hide');
+})
+
+const familyForm = document.getElementsByClassName('form-fam-meal');
+const familyMeal = document.querySelector('[value="Family Burger Meal"]');
+const submitBtn = document.getElementsByClassName('submit-family-meal');
+
+familyMeal.addEventListener('click', _ => {
+    familyForm[0].classList.toggle('hide');    
+})
+submitBtn[0].addEventListener('click', _ => {
+    familyForm[0].classList.toggle('hide');
+    alert('Item added to cart');
+})
+
+// Updating copyright year
+
+let year = document.getElementById('year');
+let nd = new Date();
+year.innerText = nd.getFullYear();
--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,31 @@
-const cta = document.getElementsByClassName('cta')[0];
-const heroSection = document.getElementsByClassName('hero-container')[0];
-const menu = document.getElementsByClassName('menu')[0];
 
-cta.addEventListener('click', _ => {
-    menu.classList.toggle('hide');
-    heroSection.classList.toggle('hide');
-})
 
-const year = document.getElementById('year');
-const nd = new Date();
-year.innerText = nd.getFullYear();
+// Adding items to cart
+
+var item = document.getElementsByClassName('menu-item-name');
+var itemPrice = document.getElementsByClassName('menu-item-price');
+var row = document.getElementById('cart-row');
+
+var x;
+var y;
+var itemsList;
+
+for (x = 0; x < item.length; x++) {
+    var itemName = item[x].value
+    var itemPriceText = itemPrice[x].textContent
+    itemsList = {
+        "name": itemName,
+        "price": itemPriceText
+    };
+    // console.log(itemsList)
+    // let addedItemToCart = row.createElement('tr')
+    // let newRow = addedItemToCart.createElement = ` <td class="item-name cart-item">${itemsList.name}</td>
+    // <td><input class="quantity-value" type="number" min="1"></td>
+    // <td class="price-value">${itemsList.price}</td>
+    // <td><input class="clear-btn" type="button" value="&times;"></td>`;
+    // addedItemToCart.appendChild(newRow);
+    // console.log(newRow)
+}
+
+
+
