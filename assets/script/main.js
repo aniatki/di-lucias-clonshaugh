0e59515e1dcb8185bc88ca83911bb94318f0fa96
Hasan Atki <djamblers@gmail.com>
2022-01-30 14:59:15 +0000

    add list element from any button clicked to localStorage for access in the shopping cart page

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,121 +1,71 @@
-// Showing / Hiding elements
+// Updating copyright year WORKS
+
+let year = document.getElementsByClassName('year')[0];
+year.innerText = new Date().getFullYear();
+
+// Removing items from cart WORKS
+
+const clearButtons = document.getElementsByClassName('clear-btn');
+for (let i = 0; i < clearButtons.length; i++) {
+    let button = clearButtons[i]
+    button.addEventListener('click', function (event) {
+        let buttonClicked = event.target
+        buttonClicked.parentElement.remove()
+    })
+}
+
+// Showing / Hiding elements WORKS
 
 const cta = document.getElementsByClassName('cta');
 const heroSection = document.getElementsByClassName('hero-container');
 const menu = document.getElementsByClassName('menu');
-const cart = document.getElementsByClassName('cart-container');
-
-cta[0].addEventListener('click', _ => {
-    menu[0].classList.toggle('hide');
-    heroSection[0].classList.toggle('hide');
-    cart[0].classList.toggle('hide');
-})
-
 const familyForm = document.getElementsByClassName('form-fam-meal');
 const familyMeal = document.getElementById('family-meal');
 const submitBtn = document.getElementsByClassName('submit-family-meal');
 
-familyMeal.addEventListener('click', _ => {
+cta[0].addEventListener('click', function (event) {
+    menu[0].classList.toggle('hide');
+    heroSection[0].classList.toggle('hide');
+})
+
+familyMeal.addEventListener('click', function (event) {
     familyForm[0].classList.toggle('hide');    
 })
-submitBtn[0].addEventListener('click', _ => {
+submitBtn[0].addEventListener('click', function (event) {
     familyForm[0].classList.toggle('hide');
     alert('Item added to cart');
-})
-
-// Updating copyright year
-
-let year = document.getElementById('year');
-let nd = new Date();
-year.innerText = nd.getFullYear();
-
-// Removing items from cart WORKS
-
-let clearBtn = document.getElementsByClassName('clear-btn');
-for (var i = 0; i < clearBtn.length; i++) {
-    let setClearBtn = clearBtn[i];
-    setClearBtn.addEventListener('click', function (event) {
-        let buttonClicked = event.target;
-        buttonClicked.parentElement.remove();
-    });
-}
-
-// Subtotal
-
-function calculateSubtotal() {
-    
-}
-
-// VAT
-
-function calculateVat() {
-    
-}
-
-// Total
-
-function calculateTotal() {
-    
-}
-// Pay 
-
-document.getElementById('pay').addEventListener('click', function(event) {
-    event.preventDefault()
-    alert('Thank you for your purchase!')
-    let cartRow = document.getElementsByClassName('cart-row');
-    for (var i = 0; i < cartRow.length; i*2) {
-        cartRow[i].remove()
-    }
 })
 
 // Get item name and price(number) WORKS
 
 let btn = document.getElementsByClassName('menu-item-name');
-let itemPrice = document.getElementsByClassName('menu-item-price');
 
 for (var i = 0; i < btn.length; i++) {
     let setBtn = btn[i];
-    setBtn.addEventListener('click', function (event) {
-        let btnClicked = event.target;
-        var itemName = btnClicked.innerText;
-        var itemPriceN = parseFloat(btnClicked.nextElementSibling.innerText.replace('€', ''));
-        console.log(itemName)
-        console.log(itemPriceN)
-    })
+    setBtn.addEventListener('click', setLocalStorage)
+    setBtn.addEventListener('mouseup', addToCart)
 }
 
-// Kyle Cooke
-
-var addToCartButtons = document.getElementsByClassName('menu-item-name')
-for (var i = 0; i < addToCartButtons.length; i++) {
-    var button = addToCartButtons[i]
-    button.addEventListener('click', addToCartClicked)
+function setLocalStorage (event) {
+    let btnClicked = event.target;
+    var itemName = btnClicked.innerText;
+    var itemPrice = parseFloat(btnClicked.nextElementSibling.innerText.replace('€', ''));
+    
+    localStorage.setItem("item", `
+    <li class="cart-row>
+        <p class="item-name">${itemName}</p>
+        <input class="quantity-value right-align" type="number" value="1" min="1">
+        <button class="clear-btn">&times;</button>
+        <p class="price-value right-align">€ ${itemPrice}</p>
+    </li>
+    `
+    )
 }
 
-function addToCartClicked(event) {
-    var button = event.target
-    var itemName = button.getElementsByClassName('menu-item-name')[0].innerText
-    addItemToCart(itemName, itemPriceN)
+function addToCart (event) {
+    let indexList = document.getElementById('list-in-index')
+    let newRow = document.createElement('li')
+    newRow.innerHTML = `${localStorage.item}`;
+    newRow.classList.add('cart-row')
 }
-
-
-
-
-// function addItemToCart(itemName, itemPriceN) {
-//     let tBody = document.getElementById('tableBody');
-//     var cartRow = document.createElement('li')
-//     cartRow.classList.add('cart-row')
-//     var cartRowContents =`
-//     <li>
-//         <p class="item-name">${itemName}</p> 
-//         <input class="quantity-value right-align" type="number" value="1" min="1">
-//         <button class="clear-btn">&times;</button> 
-//         <p class="price-value right-align">${itemPriceN}</p>
-//     </li>
-//     `;
-//     cartRow.innerHTML = cartRowContents
-//     tBody.append(cartRow)
-//     // cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-//     // cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-// }
 
---
+++ assets/script/shop.js
@@ -0,0 +1,5 @@
+const cartList = document.getElementById('tableBody');
+let newRow = document.createElement('li')
+document.body.append = `${localStorage.item}`
+console.log(localStorage.item)
+console.log(newRow.innerHTML)
