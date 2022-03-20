77688691f3a1723bb4adde8dceef1aa669017a78
Hasan Atki <djamblers@gmail.com>
2022-03-20 11:51:23 +0000

    Add functionality to hamburger menu

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,3 +2,9 @@
 
 let year = document.getElementsByClassName('year')[0];
 year.innerText = new Date().getFullYear();
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('.nav');
+hamburgerMenu.addEventListener('click', function (event) {
+    nav.classList.toggle('hidden')
+})
