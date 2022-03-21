4817704b7daa7f927a6afb3deb84a3422bee1d28
Hasan Atki <djamblers@gmail.com>
2022-03-21 02:15:11 +0000

    Add menu object to js file

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,17 @@ const nav = document.querySelector('.nav');
 hamburgerMenu.addEventListener('click', function (event) {
     nav.classList.toggle('hidden')
 })
+
+var menuCardHeader
+
+
+function populateHtml() {
+    `<section class="menu-card">
+        <h2 class="menu-card-header">${menuCardHeader}</h2>
+        <button class="menu-button">
+            <span class="menu-item-name">Chips</span>
+            <span class="menu-item-price">3.40</span>
+        </button>
+        <p class="allergen-info">Contains: <span>Gluten, Dairy, Egg</span></p>
+    < /section>`
+}
