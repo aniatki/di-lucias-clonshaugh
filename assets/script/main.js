15d01bc19ba5f8352e7f42c68ab9838a431eb774
Hasan Atki <djamblers@gmail.com>
2022-04-14 02:50:03 +0000

    Implement js function for interacting on two different pages

--- assets/script/main.js
+++ assets/script/main.js
@@ -14,44 +14,50 @@ for (let i = 0; i < clearButtons.length; i++) {
     })
 }
 
-// Showing / Hiding elements
+if (document.querySelector(".menu")) {
+    // Showing / Hiding elements
 
-const cta = document.getElementsByClassName('cta');
-const heroSection = document.getElementsByClassName('hero-container');
-const menu = document.getElementsByClassName('menu');
-const familyForm = document.getElementsByClassName('form-fam-meal');
-const familyMeal = document.getElementById('family-meal');
-const submitBtn = document.getElementsByClassName('submit-family-meal');
+    const cta = document.getElementsByClassName('cta');
+    const heroSection = document.getElementsByClassName('hero-container');
+    const menu = document.getElementsByClassName('menu');
+    const familyForm = document.getElementsByClassName('form-fam-meal');
+    const familyMeal = document.getElementById('family-meal');
+    const submitBtn = document.getElementsByClassName('submit-family-meal');
 
-cta[0].addEventListener('click', function (event) {
-    menu[0].classList.toggle('hide');
-    heroSection[0].classList.toggle('hide');
-})
+    cta[0].addEventListener('click', function (event) {
+        menu[0].classList.toggle('hide');
+        heroSection[0].classList.toggle('hide');
+    })
 
-familyMeal.addEventListener('click', function (event) {
-    familyForm[0].classList.toggle('hide');
-})
-submitBtn[0].addEventListener('click', function (event) {
-    familyForm[0].classList.toggle('hide');
-    alert('Item added to cart');
-})
+    familyMeal.addEventListener('click', function (event) {
+        familyForm[0].classList.toggle('hide');
+    })
+    submitBtn[0].addEventListener('click', function (event) {
+        familyForm[0].classList.toggle('hide');
+        alert('Item added to cart');
+    })
 
-// Get item name and price
+    // Get item name and price
 
-let btn = document.getElementsByClassName('menu-item-name');
+    let articles= document.getElementsByClassName("menu-item-card")
 
-for (var i = 0; i < btn.length; i++) {
-    let setBtn = btn[i];
-    setBtn.setAttribute('title', "Add to cart");
-    
-    setBtn.addEventListener('click', setLocalStorage);
-    
-    function setLocalStorage(event) {
-        let btnClicked = event.target;
-        var itemName = btnClicked.innerText;
-        var itemPrice = btnClicked.nextElementSibling.innerText.replace('€', '');
-        
-        localStorage.setItem("itemName", itemName);
-        localStorage.setItem("itemPrice", itemPrice);
-    }    
+    for (let i = 0; i < articles.length; i++) {
+        let article = articles[i]
+        let itemNames = article.querySelectorAll(".menu-item-name")
+        for (let j = 0; j < itemNames.length; j++) {
+            let itemName = itemNames[j]
+            itemName.addEventListener('click', handleButtonClick)
+        }
+    }
+
+    function handleButtonClick(event) {
+        let name = event.target.innerText
+        let price = parseFloat(event.target.nextElementSibling.innerText.replace("€ ", ""))
+        return name, price
+    }
+}
+// Create list items for cart page
+
+if (document.querySelector('.cart-container')) {
+    // Code for adding items to the cart
 }
